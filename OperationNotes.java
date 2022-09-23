04b921ced833da734ef463e8fcd4a0e492fc0514
HahaLNo <faithwilliams23@winchendon.org>
2022-09-23 13:56:57 -0400

    L 2.0 hate being sick fr

---
+++ OperationNotes.java
@@ -0,0 +1,20 @@
+public class OperationNotes {
+  public static void main(String[] args){
+    System.out.println(2 + 3); // addition 
+    System.out.println(2 - 3); // subtraction 
+    System.out.println(2 * 3); // multiplication
+    System.out.println(2/3); // division
+    System.out.println(7%3); // remainder, modulo, mod
+    // compound opperators
+
+    int x = 5;
+    x += 1; // add 1 to the variable
+    System.out.println(x);
+    x++; // another wat to add 1; increment 
+    System.out.println(x);
+    x--; // subtract 1, decrement
+    System.out.println(x);
+    x*= 6;
+    System.out.println(x);
+}
+}
---
+++ hw_6.java
@@ -0,0 +1,11 @@
+public class hw_6 {
+    public static void main (String[] arg){
+        int tShirt = 22;
+        System.out.println("The t-shirt costs"+ tShirt);
+        tShirt += 1;
+        System.out.println("A personalized t-shirt costs" +tShirt);
+        tShirt--;
+        System.out.println("Without personalization, the t-shirt costs" +tShirt);
+
+    }
+}
