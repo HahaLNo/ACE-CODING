f268faeb6a69493f8bf543a7b121e46092d6ba1e
HahaLNo <faithwilliams23@winchendon.org>
2022-09-27 13:42:46 -0400

    l

---
+++ DailySip_1.java
@@ -0,0 +1,13 @@
+public class DailySip_1 {
+    public static void main (String[] args){
+        int score;
+        score = 0;
+        System.out.print("The score is ");
+        System.out.println(score); 
+
+        String name = "Naomi";
+        System.out.println("Hi " + name);
+
+    }
+
+}
