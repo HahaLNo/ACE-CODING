b1034ae53c2d54cc3d0b5df1db8f9ac695ba232b
HahaLNo <113379256+HahaLNo@users.noreply.github.com>
2023-03-08 14:10:16 -0500

    boop

---
+++ AccessingArrayValues.java
@@ -0,0 +1,20 @@
+public class AccessingArrayValues {
+  public static void main(String[] args){
+    
+  String[] celebrityName = {"Jenna Ortega", "Rita Moreno", "Lucy Liu", "Samara Weaving", "Bella Ramsey"};
+  int[] celebrityAge = {20, 91, 54, 31, 19};
+  double[] celebrityHeight = {1.55, 1.57, 1.6, 1.65, 1.55};
+  
+  System.out.println(celebrityName[0] +  " is the best actress ever " + celebrityAge[0] + celebrityHeight[0]);
+  System.out.println(celebrityName[1] + " is a awesome actress " + celebrityAge[1] + celebrityHeight [1]);
+  System.out.println(celebrityName[2] + " is a great actress " + celebrityAge[2] + celebrityHeight[2]);
+  System.out.println(celebrityName[3] + " is a cool actress " + celebrityAge[3] + celebrityHeight[3]);
+  System.out.println(celebrityName[4] + " plays a very gay character " + celebrityAge[4] + celebrityHeight[4]);
+ 
+  System.out.println(celebrityName. length);
+  System.out.println(celebrityAge. length);
+  System.out.println(celebrityHeight. length);
+
+}
+
+
