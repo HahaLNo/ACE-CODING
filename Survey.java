a36db27f35e4f166df95ac5dbafb274d31da01a0
HahaLNo <113379256+HahaLNo@users.noreply.github.com>
2023-04-21 10:26:02 -0400

    Surveyyy

---
+++ Survey.java
@@ -0,0 +1,11 @@
+public class Survey {
+    public static void main(String [] args){
+        boolean[][] survey = new boolean[4][5];
+
+        survey = {{true, false, false, false, false}, 
+                  {true, false, false, false, false}, 
+                  {true, false, false, false, false}
+                  {true, false, false, false, false}
+                  {true, false, false, false, false}}
+    }
+}
